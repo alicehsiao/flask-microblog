433c08de3544829b8d18e57f16aecd7f8463b647
Alice Hsiao <ahsiao@tableau.com>
2019-10-17 09:07:31 -0700

    First commit

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Alice'}
+    posts = [
+        {
+            'author': {'username': 'Ana'},
+            'body': 'Beautiful day in Berkeley!'
+        },
+        {
+            'author': {'username': 'Joyce'},
+            'body': 'China, here I come!'
+        },
+        {
+            'author': {'username': 'Sherry'},
+            'body': 'Finally back in my own bed in Taiwan!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
